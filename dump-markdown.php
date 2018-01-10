b5133679b01ec75ba682bd8a0e93cd507b84cd0b
Federico Thiella <fthiella@gmail.com>
2018-01-10 22:50:17 +0100

    fix #3
    removed some non-necessary code and functions

--- dump-markdown.php
+++ dump-markdown.php
@@ -8,20 +8,9 @@
 */
 class AdminerDumpMarkdown {
 	/** @access protected */
-	var $database = false;
-	
+
 	function dumpFormat() {
 		return array('markdown' => 'Markdown');
-	}
-
-	function dumpTable($table, $style, $is_view = false) {
-		if ($_POST["format"] == "markdown") {
-			return true;
-		}
-	}
-
-	function _database() {
-		echo "\n";
 	}
 
 	function dumpData($table, $style, $query) {
@@ -52,12 +41,6 @@ class AdminerDumpMarkdown {
 		}
 
 		if ($_POST["format"] == "markdown") {
-			if ($this->database) {
-				echo "\n";
-			} else {
-				$this->database = true;
-				register_shutdown_function(array($this, '_database'));
-			}
 			$connection = connection();
 			$result = $connection->query($query, 1);
 			if ($result) {
