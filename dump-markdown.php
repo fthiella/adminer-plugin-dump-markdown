66a60d99e61c96915dd3e0be5b3bfd1a6bd7c716
Federico Thiella <fthiella@gmail.com>
2025-03-11 22:47:18 +0100

    Update dump-markdown.php
    feat: Add Markdown escaping to data output
    feat: Use of constants
    
    Implement _escape_markdown() and _process_value() to sanitize data for Markdown output, preventing formatting issues and potential vulnerabilities.

--- dump-markdown.php
+++ dump-markdown.php
@@ -11,17 +11,54 @@
  */
 
 class AdminerDumpMarkdown {
-	private $type = 'markdown';
-	private $format = 'Markdown';
+	const type = 'markdown';
+	const format = 'Markdown';
 
-	private $markdown_chr = [
+	const markdown_chr = [
 		'space' => ' ',
 		'table' => '|',
 		'header' => '-'
 	];
 
+	const rowSampleLimit = 100;
+	const nullValue = "N/D";
+
+    const specialChars = '\\*_[](){}+-#.!|';
+
+	function _getAdminerConnection() {
+		if (function_exists('Adminer\connection')) {
+			return Adminer\connection();
+        } elseif (function_exists('connection')) {
+            return connection();
+        }
+    }
+
+    function _escape_markdown($value) {
+    	$special_chars = '\\*_[](){}+-#.!|';
+    	$escaped_value = "";
+    	$value = strval($value);
+    	$value = utf8_decode($value);
+
+        for ($i = 0; $i < strlen($value); $i++) {
+            $char = $value[$i];
+            if (strpos(self::specialChars, $char) !== false) {
+            	$escaped_value .= '\\' . $char;
+            } else {
+                $escaped_value .= $char;
+            }
+        }
+        return $escaped_value;
+    }
+
+    function _process_value($value) {
+    	if($value === null) {
+    		return self::nullValue;
+    	}
+    	return $this->_escape_markdown($value);
+    }
+
 	function _format_value($s, $l, $c) {
-		return (strlen(utf8_decode($s)) > $l) ? substr($s, 0, $l) : $s.str_repeat($c, $l-strlen(utf8_decode($s)));
+		return (strlen($s) > $l) ? substr($s, 0, $l) : $s.str_repeat($c, $l-strlen($s));
 	}
 
 	function _map($array, $width, $c) {
@@ -35,7 +72,7 @@ class AdminerDumpMarkdown {
 	}
 
 	function _map_mtable($array) {
-		foreach ($array as $k => &$v) $v = '-';
+		foreach ($array as $k => &$v) $v = self::markdown_chr['header'];
 		return $array;
 	}
 
@@ -44,34 +81,24 @@ class AdminerDumpMarkdown {
 	}
 
 	function _markdown_table($rows, $column_width) {
-		$content  = $this->_markdown_row($this->_map_header($rows[0]), $column_width, $this->markdown_chr['space'] . $this->markdown_chr['table'] . $this->markdown_chr['space'], $this->markdown_chr['space']) . "\n";
-		$content .= $this->_markdown_row($this->_map_mtable($rows[0]), $column_width, $this->markdown_chr['header'] . $this->markdown_chr['table'] . $this->markdown_chr['header'], $this->markdown_chr['header']) . "\n";
+		$content  = $this->_markdown_row($this->_map_header($rows[0]), $column_width, self::markdown_chr['space'] . self::markdown_chr['table'] . self::markdown_chr['space'], self::markdown_chr['space']) . "\n";
+		$content .= $this->_markdown_row($this->_map_mtable($rows[0]), $column_width, self::markdown_chr['header'] . self::markdown_chr['table'] . self::markdown_chr['header'], self::markdown_chr['header']) . "\n";
 		foreach ($rows as $row) {
-			$content .= $this->_markdown_row($row, $column_width, $this->markdown_chr['space'] . $this->markdown_chr['table'] . $this->markdown_chr['space'], $this->markdown_chr['space']) . "\n";
+			$content .= $this->_markdown_row($row, $column_width, self::markdown_chr['space'] . self::markdown_chr['table'] . self::markdown_chr['space'], self::markdown_chr['space']) . "\n";
 		}
 		return $content;
 	}
-
-	function getAdminerConnection() {
-		if (function_exists('Adminer\connection')) {
-        // Newer Adminer version
-        return Adminer\connection();
-    } elseif (function_exists('connection')) {
-        // Older Adminer version
-        return connection();
-    }
-  }
 
 	function _bool($value) {
 		return $value == 1 ? 'Yes' : 'No';
 	}
 
 	function dumpFormat() {
-		return array($this->type => $this->format);
+		return array(self::type => self::format);
 	}
 
 	function dumpDatabase($db) {
-		if ($_POST["format"] == $this->type) {
+		if ($_POST["format"] == self::type) {
 			echo '# ' . $db . "\n\n";
 			return true;
 		}
@@ -79,7 +106,7 @@ class AdminerDumpMarkdown {
 
 	/* export table structure */
 	function dumpTable($table, $style, $is_view = false) {
-		if ($_POST["format"] == $this->type) {
+		if ($_POST["format"] == self::type) {
 			echo '## ' . addcslashes($table, "\n\"\\") . "\n\n";
 
 			if ($style) {
@@ -98,7 +125,7 @@ class AdminerDumpMarkdown {
 					];
 					array_push($field_rows, $new_row);
 					foreach ($new_row as $key => $val) {
-						$field_width[$key] = max($field_width[$key], strlen(utf8_decode($new_row[$key])));
+						$field_width[$key] = max($field_width[$key], strlen($new_row[$key]));
 					}
 				}
 				echo $this->_markdown_table($field_rows, $field_width);
@@ -110,11 +137,11 @@ class AdminerDumpMarkdown {
 
 	/* export table data */
 	function dumpData($table, $style, $query) {
-		if ($_POST["format"] == $this->type) {
+		if ($_POST["format"] == self::type) {
 
 			echo "### table data\n\n";
 
-			$connection = $this->getAdminerConnection();
+			$connection = $this->_getAdminerConnection();
 
 			$result = $connection->query($query, 1);
 			if ($result) {
@@ -122,27 +149,33 @@ class AdminerDumpMarkdown {
 				$sample_rows = array();
 				$column_width = array();
 
-				while ($row = $result->fetch_assoc()) {
+				while ($raw_row = $result->fetch_assoc()) {
+					// process row for output
+					$row = [];
+					foreach($raw_row as $key => $value) {
+						$row[$key] = $this->_process_value($value);
+					}
+					// end process row
 					switch(true) {
 						case $rn==0:
 							foreach ($row as $key => $val) {
-								$column_width[$key] = strlen(utf8_decode($key));
+								$column_width[$key] = strlen($this->_process_value($key));
 							}
-						case $rn<100:
+						case $rn<self::rowSampleLimit:
 							$sample_rows[$rn]=$row;
 							foreach ($row as $key => $val) {
-								$column_width[$key] = max($column_width[$key], strlen(utf8_decode($row[$key])));
+								$column_width[$key] = max($column_width[$key], strlen($row[$key]));
 							}
 							break;
-						case $rn==100:
+						case $rn==self::$rowSampleLimit:
 							echo $this->_markdown_table($sample_rows, $column_width);
 							break;
 						default:
-							echo $this->_markdown_row($row, $column_width, " | ", " ") . "\n";
+							echo $this->_markdown_row($row, $column_width, self::markdown_chr['space'] . self::markdown_chr['table'] . self::markdown_chr['space'], self::markdown_chr['space']) . "\n";
 					}
 					$rn++;
 				}
-				if ($rn<=100) {
+				if ($rn<=self::rowSampleLimit) {
 					echo $this->_markdown_table($sample_rows, $column_width);
 				}
 				echo "\n";
@@ -152,9 +185,10 @@ class AdminerDumpMarkdown {
 	}
 
 	function dumpHeaders($identifier, $multi_table = false) {
-		if ($_POST["format"] == $this->type) {
+		if ($_POST["format"] == self::type) {
 			header("Content-Type: text/text; charset=utf-8");
 			return "md";
 		}
 	}
 }
+?>
